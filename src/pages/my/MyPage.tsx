374be6a23b634ecb96b188e7a8e7a9fb0cde08d4
JaeYoung0 <hjy8697@gmail.com>
2023-02-02 03:08:22 +0900

    Fix: MyPage conditional rendering

--- src/pages/my/MyPage.tsx
+++ src/pages/my/MyPage.tsx
@@ -3,6 +3,7 @@ import BasicLayout from '@/components/layouts/BasicLayout'
 import MyCategories from '@/components/MyCategories'
 import { useUserAtom } from '@/domain/user'
 import { useAuth } from '@/service/auth'
+import { useEffect } from 'react'
 import * as S from './style'
 
 function MyPage() {
@@ -13,9 +14,14 @@ function MyPage() {
   return (
     <BasicLayout>
       <S.UserName>
-        {userAtom.name}님, 안녕하세요! <button onClick={handleClickButton}>로그아웃</button>
-        <MyCategories />
+        {userAtom.userId ? (
+          <>
+            <span>{userAtom.name}님, 안녕하세요!</span>
+            <button onClick={handleClickButton}>로그아웃</button>
+          </>
+        ) : null}
       </S.UserName>
+      <MyCategories />
     </BasicLayout>
   )
 }
