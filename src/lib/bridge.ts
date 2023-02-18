d445c353256f8e936d7cd3d40f3ed3817c51e0a0
JaeYoung0 <hjy8697@gmail.com>
2023-02-18 19:45:38 +0900

    Refactor: useWebviewBridge

--- src/lib/bridge.ts
+++ src/lib/bridge.ts
@@ -1,3 +1,5 @@
+import useDialog from '@/service/dialogAdapter'
+import { PracticeItem, usePracticeItemAtom } from '@/domain/practice'
 declare global {
   interface Window {
     ReactNativeWebView: {
@@ -6,10 +8,53 @@ declare global {
   }
 }
 
-export class NativeBridge {
-  constructor() {}
+type CreatePraticeDialogMessage = {
+  source: 'juniNative'
+  type: '@open/CreatePracticeDialog'
+  payload: { startTime: string; endTime: string }
+}
 
-  openTimer = () => {
+export type Message = WebviewMessage | OtherMessage
+
+export type WebviewMessage = CreatePraticeDialogMessage
+
+type OtherMessage = {
+  source: string
+}
+
+export function isWebviewMessage(
+  message: WebviewMessage | OtherMessage
+): message is WebviewMessage {
+  return (message as WebviewMessage).source === 'juniNative'
+}
+
+export default function useWebviewBridge() {
+  const [practiceItem, setPracticeItem] = usePracticeItemAtom()
+
+  const { openDialog } = useDialog()
+
+  const openCreatePracticeDialog = ({
+    startTime,
+    endTime,
+  }: Pick<PracticeItem, 'startTime' | 'endTime'>) => {
+    setPracticeItem({ ...practiceItem, startTime, endTime })
+    openDialog({ variant: 'CreatePracticeDialog', props: {} })
+  }
+
+  const openTimer = () => {
     window.ReactNativeWebView.postMessage('openTimer')
   }
+
+  const handleMessage = (e: MessageEvent<WebviewMessage>) => {
+    if (e.data.source !== 'juniNative') return
+
+    alert(JSON.stringify(e.data))
+
+    if (e.data.type === '@open/CreatePracticeDialog') {
+      const { startTime, endTime } = e.data.payload
+
+      openCreatePracticeDialog({ startTime, endTime })
+    }
+  }
+  return { openTimer, handleMessage }
 }
